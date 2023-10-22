7ecf0ab468ff8cc96d3142cec59afedf41a2e717
后海 <2743914896@qq.com>
2023-10-23 02:40:55 +0800

    enum

---
+++ src/main/kotlin/com/huawei/cangjie/psi/stubs/impl/CangJieEnumStubImpl.kt
@@ -0,0 +1,49 @@
+package com.huawei.cangjie.psi.stubs.impl
+
+import com.huawei.cangjie.name.ClassId
+import com.huawei.cangjie.name.FqName
+import com.huawei.cangjie.psi.CjClass
+import com.huawei.cangjie.psi.CjEnum
+import com.huawei.cangjie.psi.stubs.CangJieClassStub
+import com.huawei.cangjie.psi.stubs.CangJieEnumStub
+import com.huawei.cangjie.psi.stubs.elements.CjClassElementType
+import com.huawei.cangjie.psi.stubs.elements.CjEnumElementType
+import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.StubElement
+import com.intellij.util.io.StringRef
+import java.util.ArrayList
+
+
+open class CangJieEnumStubImpl(
+    type: CjEnumElementType,
+    parent: StubElement<out PsiElement>?,
+    private val qualifiedName: StringRef?,
+    private val classId: ClassId?,
+    private val name: StringRef?,
+    private val superNames: Array<StringRef>,
+
+    private val isLocal: Boolean,
+    private val isTopLevel: Boolean,
+) : CangJieStubBaseImpl<CjEnum>(parent, type), CangJieEnumStub {
+
+    override fun getFqName(): FqName? {
+        val stringRef = StringRef.toString(qualifiedName) ?: return null
+        return FqName(stringRef)
+    }
+
+
+    override fun isLocal() = isLocal
+    override fun getName() = StringRef.toString(name)
+
+    override fun getSuperNames(): List<String> {
+        val result = ArrayList<String>()
+        for (ref in superNames) {
+            result.add(ref.toString())
+        }
+        return result
+    }
+
+    override fun getClassId(): ClassId? = classId
+
+    override fun isTopLevel() = isTopLevel
+}
