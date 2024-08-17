85ffbb58c69fc589c9825811c6333e20140e2b67
后海 <2743914896@qq.com>
2024-08-18 00:17:46 +0800

    导入语句检查 标准库索引 依赖索引

---
+++ src/main/kotlin/com/huawei/cangjie/descriptors/impl/AbstractVariableDescriptor.kt
@@ -0,0 +1,105 @@
+package com.huawei.cangjie.descriptors.impl
+
+import com.huawei.cangjie.descriptors.*
+import com.huawei.cangjie.descriptors.annotations.Annotations
+import com.huawei.cangjie.name.Name
+import com.huawei.cangjie.types.CangJieType
+import com.huawei.cangjie.types.util.shouldBeUpdated
+
+
+abstract class AbstractVariableDescriptor(
+    containingDeclaration: DeclarationDescriptor,
+    annotations: Annotations,
+    name: Name,
+    private var _outType: CangJieType?,
+    source: SourceElement
+
+) : DeclarationDescriptorNonRootImpl(containingDeclaration, annotations, name, source),
+    VariableDescriptorBase {
+
+    override val original: VariableDescriptorBase = super.original as VariableDescriptorBase
+    override fun getContextReceiverParameters(): List<ReceiverParameterDescriptor> {
+        return emptyList()
+    }
+
+    open fun setOutType(outType: CangJieType) {
+        assert(this._outType == null || this._outType.shouldBeUpdated())
+        this._outType = outType
+    }
+
+    override fun getVisibility() = DescriptorVisibilities.LOCAL
+
+    override fun getReturnType(): CangJieType {
+        return type
+    }
+
+    override val isConst:Boolean get() {
+        return false
+    }
+    override fun getDispatchReceiverParameter(): ReceiverParameterDescriptor? {
+        return null
+    }
+
+    override fun hasStableParameterNames(): Boolean {
+        return false
+    }
+
+//    fun setType(
+//        _outType: CangJieType,
+//        @ReadOnly typeParameters: List<TypeParameterDescriptor?>,
+//        dispatchReceiverParameter: ReceiverParameterDescriptor?,
+//        extensionReceiverParameter: ReceiverParameterDescriptor?,
+//        contextReceiverParameters: List<ReceiverParameterDescriptor?>
+//    ) {
+//        setOutType(_outType)
+//
+//        this.typeParameters = ArrayList<TypeParameterDescriptor>(typeParameters)
+//
+//        this.extensionReceiverParameter = extensionReceiverParameter
+//        this.dispatchReceiverParameter = dispatchReceiverParameter
+//        this.contextReceiverParameters = contextReceiverParameters
+//    }
+
+    companion object {
+//        fun create(
+//            containingDeclaration: DeclarationDescriptor,
+//            annotations: Annotations,
+//            modality: Modality,
+//            visibility: DescriptorVisibility,
+//            isVar: Boolean,
+//            name: Name,
+//            kind: CallableMemberDescriptor.Kind,
+//            source: SourceElement,
+//            lateInit: Boolean,
+//            isConst: Boolean,
+//            isExpect: Boolean,
+//            isActual: Boolean,
+//            isExternal: Boolean,
+//            isDelegated: Boolean
+//        ): AbstractVariableDescriptor {
+//            return AbstractVariableDescriptor(
+//                containingDeclaration, null, annotations,
+//                modality, visibility, isVar, name, kind, source, lateInit, isConst,
+//                isExpect, isActual, isExternal, isDelegated
+//            )
+//        }
+
+    }
+
+    override fun getType(): CangJieType {
+        return _outType!!
+    }
+
+    override fun getExtensionReceiverParameter(): ReceiverParameterDescriptor? {
+        return null
+    }
+
+    override fun getTypeParameters(): List<TypeParameterDescriptor> {
+        return emptyList<TypeParameterDescriptor>()
+    }
+
+    override fun getValueParameters(): List<ValueParameterDescriptor> {
+        return emptyList()
+    }
+
+}
