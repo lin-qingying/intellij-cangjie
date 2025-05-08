da035c43c538a007be3517f3274f77e2fa944825
后海 <2743914896@qq.com>
2025-05-08 10:49:11 +0800

    Add new configuration files and live templates for CangJie
    This commit introduces JSON configuration files (a.json and b.json), intention descriptions, and live template definitions for the CangJie IDE integration. These changes enhance the development experience by providing pre-defined code templates and necessary initialization configurations.

--- build.gradle.kts
+++ build.gradle.kts
@@ -246,9 +246,8 @@ allprojects {
         withType<PatchPluginXmlTask> {
             sinceBuild.set(ideVersion)
             untilBuild.set("$ideVersion.*")
-
             pluginVersion.set(cangjiePluginVersion)
-
+//
 //            sinceBuild.set("243")
 //            untilBuild.set("253.*")
 //            pluginVersion.set(prop("pluginVersion"))
