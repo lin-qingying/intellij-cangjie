72f8fb7b1192e8a2f3ca09d00b3f82e703ce4c81
后海 <2743914896@qq.com>
2025-05-25 22:43:47 +0800

    refactor(introduce): 更新引入变量功能的错误提示
    - 修改了 showErrorHint 函数的错误代码参数
    - 新增了多个错误代码的详细说明文档
    - 创建了错误代码目录的索引文件

--- src/main/kotlin/cn/cangnova/cangjie/ide/refactoring/introduce/CangJieIntroduceVariableHandler.kt
+++ src/main/kotlin/cn/cangnova/cangjie/ide/refactoring/introduce/CangJieIntroduceVariableHandler.kt
@@ -42,7 +42,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsContexts
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.refactoring.HelpID
+
 import com.intellij.refactoring.RefactoringActionHandler
 import com.intellij.refactoring.util.CommonRefactoringUtil
 import com.intellij.util.SmartList
@@ -111,7 +111,7 @@ abstract class CangJieIntroduceVariableHandler : RefactoringActionHandler {
          * @param message 要显示的错误消息
          */
         fun showErrorHint(project: Project, editor: Editor?, @NlsContexts.DialogMessage message: String) {
-            CommonRefactoringUtil.showErrorHint(project, editor, message, INTRODUCE_VARIABLE, HelpID.INTRODUCE_VARIABLE)
+            CommonRefactoringUtil.showErrorHint(project, editor, message, INTRODUCE_VARIABLE,  "refactoring.introduceVariable")
         }
 
         /**
