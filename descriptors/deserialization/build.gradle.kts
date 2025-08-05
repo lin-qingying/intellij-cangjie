40424661c6465229a78bb22fe5bdf186383adc45
后海 <2743914896@qq.com>
2025-08-06 00:35:16 +0800

    feat(metadata): 增加元数据

---
+++ descriptors/deserialization/build.gradle.kts
@@ -0,0 +1,12 @@
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation(project(":psi"))
+    implementation(project(":util"))
+    implementation(project(":descriptors"))
+
+    implementation(project(":common"))
+}
+
---
+++ metadata/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm")
+    id("io.netifi.flatbuffers") version "1.0.7"
+}
+
+dependencies {
+    implementation("com.google.flatbuffers:flatbuffers-java:25.2.10")
+
+}
+
+// FlatBuffers配置
+flatbuffers {
+    flatcPath = project(":metadata").projectDir.path + "/flatbuffers/flatc.exe" // 使用本地的flatc.exe
+    language = "kotlin"
+    flatBuffersVersion = "25.2.10"
+}
+tasks.register<io.netifi.flatbuffers.plugin.tasks.FlatBuffers>("generateKotlinFlatBuffers") {
+    inputDir = file("flatbuffers")
+    outputDir = file(project(":metadata").projectDir .path + "/gen")
+    language = "kotlin"
+    extraArgs = "--gen-mutable --gen-object-api"
+}
+
+tasks.compileKotlin {
+    dependsOn(/*"generateFlatBuffers", */"generateKotlinFlatBuffers")
+}
+sourceSets {
+    main {
+        kotlin {
+            srcDirs("gen")
+        }
+    }
+}
