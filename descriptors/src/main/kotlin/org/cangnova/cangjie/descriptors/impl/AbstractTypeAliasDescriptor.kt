954429c0436d41f428465dd7f9f32effcca03099
LinQIngYing <2743914896@qq.com>
2025-09-16 20:29:25 +0800

    feat(migration): add XML configuration files for migration states and copyright management

--- descriptors/src/main/kotlin/org/cangnova/cangjie/descriptors/impl/AbstractTypeAliasDescriptor.kt
+++ descriptors/src/main/kotlin/org/cangnova/cangjie/descriptors/impl/AbstractTypeAliasDescriptor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 LinQingYing. and contributors.
+ * Copyright 2025 LinQingYing. and contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -109,7 +109,7 @@ abstract class AbstractTypeAliasDescriptor(
             cangjieTypeRefiner?.refineDescriptor(this)?.defaultType
         }
 
-            cangjieTypeRefiner.refineDescriptor(this)?.defaultType
+    override val typeConstructor = object : TypeConstructor {
 
         override val declarationDescriptor: TypeAliasDescriptor
             get() =
